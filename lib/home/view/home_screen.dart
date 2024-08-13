aeccb7f5c62327fc2d2dc2eebee781b4460eb76e
huutruong <huutruong15599@gmail.com>
2024-08-13 18:21:21 +0700

    republish

---
+++ lib/home/view/home_screen.dart
@@ -0,0 +1,193 @@
+import 'package:counting_with_bloc/home/bloc/home_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  static Route<void> route() {
+    return MaterialPageRoute<void>(builder: (_) => const HomeScreen());
+  }
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController inputController = TextEditingController();
+
+  @override
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<HomeBloc, HomeState>(
+      builder: (context, state) {
+        inputController = TextEditingController(text: state.input.toString());
+
+        Future<void> addNumberDialog() {
+          return showDialog<void>(
+            context: context,
+            builder: (BuildContext context) {
+              return BlocBuilder<HomeBloc, HomeState>(
+                builder: (context, state) {
+                  //update input controller when state change
+                  
+                  inputController.text = state.input.toString();
+                  return AlertDialog(
+                    title: const Text('Basic dialog title'),
+                    content: SizedBox(
+                      width: double.infinity,
+                      child: Row(
+                        children: [
+                          IconButton(
+                              onPressed: () {
+                                context.read<HomeBloc>().add(DecreaseEvent());
+                              },
+                              icon: const Icon(Icons.remove)),
+                          Expanded(
+                            child: TextFormField(
+                                controller: inputController,
+                                onChanged: (value) {
+                                  context.read<HomeBloc>().add(
+                                      OnchangeNumberEvent(
+                                          number: int.parse((value.isNotEmpty)
+                                              ? value
+                                              : "0")));
+                                },
+                                keyboardType: TextInputType.number,
+                                decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  labelText: 'Enter a number',
+                                )),
+                          ),
+                          IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  context.read<HomeBloc>().add(IncreaseEvent());
+                                });
+                              },
+                              icon: const Icon(Icons.add)),
+                        ],
+                      ),
+                    ),
+                    actions: <Widget>[
+                      TextButton(
+                        style: TextButton.styleFrom(
+                          textStyle: Theme.of(context).textTheme.labelLarge,
+                        ),
+                        child: const Text('Cancel'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                      TextButton(
+                        style: TextButton.styleFrom(
+                          textStyle: Theme.of(context).textTheme.labelLarge,
+                        ),
+                        child: const Text('Add'),
+                        onPressed: () {
+                          context.read<HomeBloc>().add(AddNumberEvent());
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+          );
+        }
+
+        Future<void> deleleNumber(int index) {
+          return showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: const Text("Delete Number"),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text("Cancel"),
+                ),
+                TextButton(
+                  onPressed: () =>{
+                    Navigator.pop(context),
+                    context.read<HomeBloc>().add(DeleteNumberEvent(index))
+
+                  },
+                  child: Text("Delete $index"),
+                ),
+              ],
+            ),
+          );
+        }
+
+        return Scaffold(
+          appBar: AppBar(
+            leading: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(Icons.arrow_back_ios)),
+            title: const Text("Home Screen"),
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              addNumberDialog();
+            },
+            child: const Icon(Icons.add),
+          ),
+          body: Container(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    "Welcome to Home Screen",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    "History",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  ListView.separated(
+                    scrollDirection: Axis.vertical,
+                    physics: const BouncingScrollPhysics(),
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text("Item ${state.result![index]}"),
+                        trailing: IconButton(
+                            onPressed:() => deleleNumber(index),
+                            icon: Icon(Icons.delete, color: Colors.grey[800])),
+                      );
+                    },
+                    separatorBuilder: (context, index) => const SizedBox(
+                      height: 10,
+                    ),
+                    itemCount: state.result!.length,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
